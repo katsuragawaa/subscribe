afa107d2b4ab1276e7e535b7570b292e78b95c92
katsuragawa <andre.katsuragawa@gmail.com>
2022-12-04 21:37:15 -0300

    refactor: move expected item inside test func

--- src/__tests__/lib.test.js
+++ src/__tests__/lib.test.js
@@ -5,17 +5,16 @@ const mockItems = [
     { name: 'item_2', quantity: 2, value: 5, type: 'Other', imported: false },
 ]
 
-const expected = {
-    items: [
-        { ...mockItems[0], total: 10.5 },
-        { ...mockItems[1], total: 11 },
-    ],
-    salesTax: 1.5,
-    totalWithoutTax: 20,
-}
-
 describe('test calculate function', () => {
     test('should return correct values', () => {
+        const expected = {
+            items: [
+                { ...mockItems[0], total: 10.5 },
+                { ...mockItems[1], total: 11 },
+            ],
+            salesTax: 1.5,
+            totalWithoutTax: 20,
+        }
         expect(calculate(mockItems)).toStrictEqual(expected)
     })
 })
